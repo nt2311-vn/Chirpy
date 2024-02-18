a54059c61b80b7f1ed667e43a6e40dc11251a184
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-18 22:46:12 +0700

    Ensure the server start at port 8080 and the mux will handle and serve file path

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("./")))
+	corsMux := middlewareCors(mux)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: corsMux,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Cannot start the server: %v", err)
+	}
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
