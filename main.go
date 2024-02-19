214d08f23e4c1b1956ed688f1c5a1d47b1ee736d
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-19 09:56:47 +0700

    Add metric handler and reset handler

--- main.go
+++ main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text-plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text-plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Counter reset"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("./")))
+
+	appHandler := http.StripPrefix("/app/", http.FileServer(http.Dir("./")))
+	mux.Handle("/app/", appHandler)
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("/metrics", apiConfig.metricsHandler)
+	mux.HandleFunc("/reset", apiConfig.resetHandler)
+
 	corsMux := middlewareCors(mux)
 
 	server := &http.Server{
