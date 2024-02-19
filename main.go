e9b73baf69dd16c9280df34fcf2c7dbb4cdae629
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-19 13:42:31 +0700

    Implement healthz path

--- main.go
+++ main.go
@@ -11,6 +11,12 @@ func main() {
 
 	corsMux := middlewareCors(mux)
 
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: corsMux,
