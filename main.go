9fa7a7151a60a75f75cf0a4808a2eba23f6eda92
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-20 14:27:35 +0700

    Restructure the server

--- main.go
+++ main.go
@@ -12,13 +12,6 @@ type apiConfig struct {
 	fileserverHits int
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits++
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	r := chi.NewRouter()
 	// mux := http.NewServeMux()
@@ -29,28 +22,6 @@ func main() {
 	r.Handle("/app/*", cfg.middlewareMetricsInc(appHandler))
 
 	corsMux := middlewareCors(r)
-
-	/*
-
-		r.Get("/api/healthz", func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte("OK"))
-		})
-
-		r.HandleFunc("/api/reset", func(w http.ResponseWriter, r *http.Request) {
-			cfg.fileserverHits = 0
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte("Counter reset"))
-		})
-
-		r.Get("/api/metrics", func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
-		})
-	*/
 
 	r.Mount("/api", apiRouter(r, cfg))
 	r.Mount("/admin", adminRouter(r, cfg))
@@ -63,21 +34,6 @@ func main() {
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("Cannot start the server: %v", err)
 	}
-}
-
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
 }
 
 func apiRouter(r *chi.Mux, cfg *apiConfig) http.Handler {
---
+++ metrics.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
