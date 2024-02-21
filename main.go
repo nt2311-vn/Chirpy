a05dd087b355252f542268928f860b8ccb510377
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-21 09:50:13 +0700

    Reorganize api and admin router to main

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -33,4 +35,86 @@ func main() {
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("Cannot start the server: %v", err)
 	}
+}
+
+func apiRouter(r *chi.Mux, cfg *apiConfig) http.Handler {
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	r.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits = 0
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Counter reset"))
+	})
+
+	r.Post("/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+		type chirpreq struct {
+			Reqmsg string `json:"body,omitempty"`
+		}
+
+		type chirpres struct {
+			Resmsg bool `json:"valid,omitempty"`
+		}
+
+		type errorres struct {
+			Errmsg string `json:"error,omitempty"`
+		}
+		decoder := json.NewDecoder(r.Body)
+		params := chirpreq{}
+
+		if err := decoder.Decode(&params); err != nil {
+			log.Printf("Error decoding json body: %v", err)
+			errormsg := errorres{
+				Errmsg: fmt.Sprintf("%v", err),
+			}
+
+			resMsg, _ := json.Marshal(errormsg)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(500)
+			w.Write(resMsg)
+		}
+
+		if len(params.Reqmsg) > 140 {
+			errormsg := errorres{
+				Errmsg: "Chirp is too long",
+			}
+			resMsg, _ := json.Marshal(errormsg)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(500)
+			w.Write(resMsg)
+
+		}
+
+		okMsg := chirpres{Resmsg: true}
+		encodeMsg, _ := json.Marshal(okMsg)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(encodeMsg)
+	})
+
+	return r
+}
+
+func adminRouter(r *chi.Mux, cfg *apiConfig) http.Handler {
+	r.Get("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf(`
+		<html>
+
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been visited %d times!</p>
+		</body>
+
+		</html>
+
+		`, cfg.fileserverHits)))
+	})
+	return r
 }
