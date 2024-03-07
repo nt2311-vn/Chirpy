d44c8c11a88245c9d2aa4d4a5d5e6776daa065de
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-07 13:15:14 +0700

    separate module design for database

---
+++ internal/database/database.go
@@ -0,0 +1,88 @@
+package database
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"sync"
+)
+
+var ErrNotExist = errors.New("resource does not exist")
+
+type DB struct {
+	path string
+	mu   *sync.RWMutex
+}
+
+type DBStructure struct {
+	Chirps      map[int]Chirp         `json:"chirps"`
+	Users       map[int]User          `json:"users"`
+	Revocations map[string]Revocation `json:"revocations"`
+}
+
+func NewDB(path string) (*DB, error) {
+	db := &DB{
+		path: path,
+		mu:   &sync.RWMutex{},
+	}
+	err := db.ensureDB()
+	return db, err
+}
+
+func (db *DB) createDB() error {
+	dbStructure := DBStructure{
+		Chirps:      map[int]Chirp{},
+		Users:       map[int]User{},
+		Revocations: map[string]Revocation{},
+	}
+	return db.writeDB(dbStructure)
+}
+
+func (db *DB) ensureDB() error {
+	_, err := os.ReadFile(db.path)
+	if errors.Is(err, os.ErrNotExist) {
+		return db.createDB()
+	}
+	return err
+}
+
+func (db *DB) ResetDB() error {
+	err := os.Remove(db.path)
+	if errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+	return db.ensureDB()
+}
+
+func (db *DB) loadDB() (DBStructure, error) {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	dbStructure := DBStructure{}
+	dat, err := os.ReadFile(db.path)
+	if errors.Is(err, os.ErrNotExist) {
+		return dbStructure, err
+	}
+	err = json.Unmarshal(dat, &dbStructure)
+	if err != nil {
+		return dbStructure, err
+	}
+
+	return dbStructure, nil
+}
+
+func (db *DB) writeDB(dbStructure DBStructure) error {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	dat, err := json.Marshal(dbStructure)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(db.path, dat, 0600)
+	if err != nil {
+		return err
+	}
+	return nil
+}
